45e415b2f58b1fd6e9e15c1e6e382338d8cc1885
denesbeck <denes.beck1@outlook.com>
2025-10-14 19:50:02 +0200

    fix: bio modal

--- app/about/_components/Bio/Bio.tsx
+++ app/about/_components/Bio/Bio.tsx
@@ -3,11 +3,11 @@ import { Heading3 } from "@/_components";
 import { BIO, BIO_START } from "../../_config/data";
 import { FaMagnifyingGlass } from "react-icons/fa6";
 import { Modal } from ".";
-import { useState } from "react";
 import { Tooltip } from "@mui/material";
+import { useRouter } from "next/navigation";
 
 const Bio = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const { push } = useRouter();
   return (
     <>
       <div className="flex overflow-x-auto flex-col gap-2 px-2 animate-text-focus max-w-[30rem]">
@@ -16,7 +16,7 @@ const Bio = () => {
             <span>Bio</span>
             <Tooltip arrow placement="right" title="Show more">
               <button
-                onClick={() => setIsOpen(true)}
+                onClick={() => push("/about?modalOpen")}
                 className="transition-all duration-200 ease-in-out cursor-pointer hover:scale-110 text-primary hover:brightness-125"
               >
                 <FaMagnifyingGlass className="text-base text-primary" />
@@ -28,7 +28,7 @@ const Bio = () => {
           {BIO_START}
         </div>
       </div>
-      {isOpen && <Modal close={() => setIsOpen(false)}>{BIO}</Modal>}
+      <Modal>{BIO}</Modal>
     </>
   );
 };
--- app/about/_components/Bio/Modal.tsx
+++ app/about/_components/Bio/Modal.tsx
@@ -1,22 +1,28 @@
 import { DarkLayout, MacOSBar } from "@/_components";
 import { ReactElement } from "react";
 import { useClickOutside } from "@/_hooks";
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 interface ModalProps {
   children: ReactElement;
-  close: () => void;
 }
 
-const Modal = ({ children, close }: ModalProps) => {
-  const ref = useClickOutside<HTMLDivElement>(close);
+const Modal = ({ children }: ModalProps) => {
+  const { push } = useRouter();
+  const ref = useClickOutside<HTMLDivElement>(() => push("/about"));
+  const searchParams = useSearchParams();
+  const isOpen = searchParams.has("modalOpen");
+
+  if (!isOpen) return;
   return (
     <DarkLayout>
       <div
         ref={ref}
-        className="flex flex-col px-4 w-max h-full sm:pb-2 sm:ring-2 ring-primary animate-slide-in-from-bottom bg-dark-900 max-w-[100dvw] font-[DepartureMono] sm:h-max sm:max-w-[640px]"
+        className="flex flex-col pt-2 w-max h-full sm:pb-2 sm:ring-2 ring-primary animate-slide-in-from-bottom bg-dark-900 max-w-[100dvw] sm:h-max sm:max-w-[640px]"
       >
         <MacOSBar close={"/about"} />
-        <div className="overflow-y-auto overflow-x-hidden pt-2 pr-4 pb-4 pl-2 h-full max-h-[calc(100dvh-85px)] lg:max-h-[calc(100dvh-170px)]">
+        <div className="overflow-y-auto overflow-x-hidden px-5 pt-3 pb-5 h-full max-h-[calc(100dvh-85px)] lg:max-h-[calc(100dvh-170px)]">
           {children}
         </div>
       </div>
