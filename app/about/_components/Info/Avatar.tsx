569f346d9922d3cc3a7bd44948bb07c8d1fc14e4
denesbeck <denes.beck1@outlook.com>
2025-10-14 19:50:02 +0200

    refactor: avatar skeleton

--- app/about/_components/Info/Avatar.tsx
+++ app/about/_components/Info/Avatar.tsx
@@ -1,15 +1,39 @@
+"use client";
 import profile from "@/../public/avatars/ghibli_avatar.png";
 import Image from "next/image";
+import { useState } from "react";
+import { Skeleton, ThemeProvider } from "@mui/material";
+import { darkTheme } from "@/theme";
 
 const Avatar = () => {
+  const [loading, setLoading] = useState(true);
+
+  const handleLoad = () => {
+    setLoading(false);
+  };
+
   return (
     <div className="hidden overflow-hidden mx-3 w-40 h-40 rounded-full ring-2 sm:block min-w-40 min-h-40 ring-primary">
+      {loading && (
+        <ThemeProvider theme={darkTheme}>
+          <Skeleton
+            variant="rounded"
+            animation="wave"
+            width={160}
+            height={160}
+            className="min-w-40 min-h-40 !bg-dark-800"
+          />
+        </ThemeProvider>
+      )}
+
       <Image
         src={profile}
         alt="profile"
         width={160}
         height={160}
-        className="w-40 h-40 rounded-full transition-all duration-200 ease-in-out hover:scale-110 min-w-40 min-h-40"
+        quality={100}
+        className="w-40 h-40 rounded-full transition-all duration-200 ease-in-out hover:scale-110 animate-text-focus min-w-40 min-h-40"
+        onLoadingComplete={() => handleLoad()}
       />
     </div>
   );
