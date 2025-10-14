b46ed6ddfb0e6f3ced55e5eb4d24cd9493c56a55
denesbeck <denes.beck1@outlook.com>
2025-10-14 19:50:02 +0200

    fix: images config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import createMDX from "@next/mdx";
 import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [new URL("https://images.credly.com/**")],
+  },
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 
