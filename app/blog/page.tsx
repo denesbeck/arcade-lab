e605f61dc475f55b456c4f6ea7c256d1df69df55
denesbeck <denes.beck1@outlook.com>
2025-10-20 14:58:50 +0200

    refactor: sort blog entries by creation date

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -21,18 +21,20 @@ const Blog = async ({
           <NoRecords />
         ) : (
           <div className="mr-6 grid h-[110%] justify-center sm:w-dvw sm:[grid-template-columns:repeat(auto-fit,minmax(33rem,0))] sm:gap-12 sm:px-10">
-            {entries.map((entry) => (
-              <BlogCard
-                key={entry.id}
-                id={entry.id}
-                title={entry.title}
-                description={entry.description}
-                date={entry.date}
-                tags={entry.tags}
-                content={entry.content}
-                cover={entry.cover}
-              />
-            ))}
+            {entries
+              .sort((a, b) => b.date.localeCompare(a.date))
+              .map((entry) => (
+                <BlogCard
+                  key={entry.id}
+                  id={entry.id}
+                  title={entry.title}
+                  description={entry.description}
+                  date={entry.date}
+                  tags={entry.tags}
+                  content={entry.content}
+                  cover={entry.cover}
+                />
+              ))}
           </div>
         )}
       </div>
