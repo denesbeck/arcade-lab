cd4a3a6fbc728245451ee8093eafaa1481bfeb60
denesbeck <denes.beck1@outlook.com>
2025-11-19 17:33:57 +0100

    style: remove animation

--- app/_components/GoBack.tsx
+++ app/_components/GoBack.tsx
@@ -22,7 +22,7 @@ const GoBack = ({ fallbackUrl }: IGoBack) => {
     <div className="flex justify-end px-6 pb-2 w-full">
       <button
         onClick={handleGoBack}
-        className="flex items-center space-x-2 underline whitespace-nowrap cursor-pointer underline-offset-4 text-dark-100 animate-text-focus hover:decoration-dashed"
+        className="flex items-center space-x-2 underline whitespace-nowrap cursor-pointer underline-offset-4 text-dark-100 hover:decoration-dashed"
       >
         <FaArrowCircleLeft />
         <span>Go back</span>
