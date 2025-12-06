df4d08c2eb8b81eb42be895581e5d506b52b9817
denesbeck <denes.beck1@outlook.com>
2025-12-06 09:57:12 +0100

    refactor: remove x

--- app/about/_config/data.tsx
+++ app/about/_config/data.tsx
@@ -19,7 +19,7 @@ import {
   FaBriefcase,
   FaLinkedin,
 } from "react-icons/fa";
-import { FaGolang, FaXTwitter } from "react-icons/fa6";
+import { FaGolang } from "react-icons/fa6";
 import { HiIdentification } from "react-icons/hi2";
 import { Emphasize } from "@/_components";
 
@@ -46,7 +46,6 @@ export const CONNECTIONS = [
     url: "https://www.linkedin.com/in/denesbeck",
     icon: FaLinkedin,
   },
-  { url: "https://x.com/DenesBeck", icon: FaXTwitter },
   {
     url: "https://www.credly.com/users/denesbeck",
     icon: SiCredly,
