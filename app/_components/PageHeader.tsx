8c71237f031d778962bba06f3ca3e1f52b1a194a
denesbeck <denes.beck1@outlook.com>
2026-03-08 19:31:31 +0100

    style: page header padding

--- app/_components/PageHeader.tsx
+++ app/_components/PageHeader.tsx
@@ -7,7 +7,7 @@ interface IPageHeader {
 
 const PageHeader = ({ title, description }: IPageHeader) => {
   return (
-    <div className="flex flex-col gap-2 py-4 px-6 animate-text-focus">
+    <div className="flex flex-col gap-2 py-4 px-2 sm:px-6 animate-text-focus">
       <Heading1>{title}</Heading1>
       <Info>{description}</Info>
     </div>
