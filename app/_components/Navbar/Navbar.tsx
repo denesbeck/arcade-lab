3d07ec68e26b5a7e664f74156ee157bbd4a81bf2
denesbeck <denes.beck1@outlook.com>
2026-08-04 20:22:18 +0200

    style: responsive design fix

--- app/_components/Navbar/Navbar.tsx
+++ app/_components/Navbar/Navbar.tsx
@@ -9,16 +9,16 @@ const Navbar = () => {
 
   return (
     <Portal>
-      <div className="flex fixed top-0 z-10 justify-center w-full pointer-events-none h-max">
-        <nav className="flex relative items-start px-6 pt-2 pb-24 w-full pointer-events-none isolate max-w-350 animate-text-focus h-max">
+      <div className="pointer-events-none fixed top-0 z-10 flex h-max w-full justify-center">
+        <nav className="pointer-events-none relative isolate flex h-max w-full max-w-350 animate-text-focus items-center px-6 pt-2 pb-24">
           <div
             aria-hidden
-            className="absolute inset-0 -z-10 backdrop-blur-md pointer-events-none mask-[linear-gradient(to_bottom,black_0%,black_18%,rgba(0,0,0,0.92)_30%,rgba(0,0,0,0.78)_42%,rgba(0,0,0,0.6)_54%,rgba(0,0,0,0.4)_66%,rgba(0,0,0,0.22)_78%,rgba(0,0,0,0.08)_90%,transparent_100%)]"
+            className="pointer-events-none absolute inset-0 -z-10 mask-[linear-gradient(to_bottom,black_0%,black_18%,rgba(0,0,0,0.92)_30%,rgba(0,0,0,0.78)_42%,rgba(0,0,0,0.6)_54%,rgba(0,0,0,0.4)_66%,rgba(0,0,0,0.22)_78%,rgba(0,0,0,0.08)_90%,transparent_100%)] backdrop-blur-md"
           />
           <Title mr />
           {pathname !== '/contact' && (
             <>
-              <div className="hidden justify-center items-center py-1 space-x-4 w-max pointer-events-auto sm:flex animate-text-focus">
+              <div className="pointer-events-auto hidden w-max animate-text-focus items-center justify-center space-x-4 py-1 md:flex">
                 {navItems.map((item) => (
                   <Navitem
                     key={item.label}
@@ -27,7 +27,7 @@ const Navbar = () => {
                   />
                 ))}
               </div>
-              <div className="flex justify-end py-2 w-full pointer-events-auto sm:hidden animate-text-focus">
+              <div className="pointer-events-auto flex w-full animate-text-focus justify-end py-2 md:hidden">
                 <Menu>
                   <>
                     {navItems.map((item) => (
